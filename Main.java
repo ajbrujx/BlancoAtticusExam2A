7aa8f09c5c2fd7fa181ec77c8dd9726147967dfd
Atticus Blanco <atticusbrujx@gmail.com>
2023-10-31 16:22:28 -0700

    Part 1 >>>NONOPERATIONAL
    >>>MODIFICATIONS
    --Main.java
      -psuedocode
    
    >>>DEFICITS
    --Main.java
      -STUB method main
      -STUB method returnOne
      -STUB method printEven
      -STUB method sumElements
      -STUB method reverse
      -STUB method fillArray
    
    >>>BUGS
    --
    
    >>>THROWS
    --

--- Main.java
+++ Main.java
@@ -6,22 +6,43 @@ public class Main {
         return;
     }
 
+
+    /*
+    * A method called, fillArray, that uses a for loop to fill the array test with random integers between
+    * the values of 1 and 10 inclusive.
+     */
     public static void fillArray(){
         return;
     }
 
+
+    /* A method called, returnOne, that will return the value of the element based on the index value passed to the method.
+     * In other words, given the method call: returnOne(3), the method will return the value of the element at index 3 in the array.
+     */
     public static void returnOne(){
         return;
     }
 
+
+    /*
+    * A method called, printEven, that uses a for loop to determine and print all the even numbers contained in the
+    * test array to the terminal window.
+    */
     public static void printEven(){
         return;
     }
 
+
+    /*
+    * (5 points) A method called, sumElements, that uses a for loop to sum all of the elements of the array, test,
+    * and prints the result to the terminal window ONE time.
+    */
     public static void sumElements(){
         return;
     }
 
+    /*
+    * A method called, reverse, that will print the contents of the array, test, in reverse order.*/
     public static void reverse(){
         return;
     }
